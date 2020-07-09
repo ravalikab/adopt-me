be2bb436f906484ac9d28e8dc3142f71fff701aa
Ravalika <25775803+ravalikab@users.noreply.github.com>
2020-07-09 17:28:05 +0530

    Pet component updated

--- src/Pet.js
+++ src/Pet.js
@@ -1,48 +1,18 @@
 import React from "react";
-export default function Pet({ name, animal, breed }) {
+export default function Pet({ name, animal, breed, media, location, id }) {
+  let hero = "http://placecorgi.com/300/300";
+  if (media.length) {
+    hero = media[0].small;
+  }
   return (
-    <div>
-      <h1>{name}</h1>
-      <h2>{animal}</h2>
-      <h2>{breed}</h2>
-    </div>
+    <a href={`/details/${id}`} className="pet">
+      <div className="image-container">
+        <img src={hero} alt={name} />
+      </div>
+      <div className="info">
+        <h1>{name}</h1>
+        <h2>{`${animal}-${breed}-${location}`}</h2>
+      </div>
+    </a>
   );
 }
-
-//   return React.createElement("div", {}, [
-//     React.createElement("h1", {}, props.name),
-//     React.createElement("h2", {}, props.animal),
-//     React.createElement("h2", {}, props.breed),
-//   ]);
-
-// const App = () => {
-// return React.createElement("div", {}, [
-//   React.createElement("h1", {id:"something"}, "Adopt Me"),
-//   React.createElement(Pet, {
-//     name: "Luna",
-//     animal: "Dog",
-//     breed: "Havanese",
-//   }),
-//   React.createElement(Pet, {
-//     name: "Pepper",
-//     animal: "Bird",
-//     breed: "Pegion",
-//   }),
-//   React.createElement(Pet, {
-//     name: "Billy",
-//     animal: "Cat",
-//     breed: "Mixed",
-//   }),
-// ]);
-
-//     return (
-//       <div>
-//         <h1 id="something">Adopt me</h1>
-//         <Pet name="Luna" animal="Dog" breed="Havanese" />
-//         <Pet name="Pepper" animal="Bird" breed="Pegion" />
-//         <Pet name="Billy" animal="cat" breed="Mixed" />
-//       </div>
-//     );
-//   };
-
-//   render(<App />, document.getElementById("root"));
--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import pet, { ANIMALS } from "@frontendmasters/pet";
+import Results from "./Results";
 import useDropdown from "./useDropdown";
 
 const SearchParams = () => {
@@ -7,6 +8,16 @@ const SearchParams = () => {
   const [breeds, setBreeds] = useState([]);
   const [animal, AnimalDropdown] = useDropdown("animal", "dog", ANIMALS);
   const [breed, BreedDropdown, setBreed] = useDropdown("breed", "", breeds);
+  const [pets, setPets] = useState([]);
+
+  async function requestPets() {
+    const { animals } = await pet.animals({
+      location,
+      breed,
+      type: animal,
+    });
+    setPets(animals || []);
+  }
 
   useEffect(() => {
     setBreeds([]);
@@ -19,8 +30,13 @@ const SearchParams = () => {
   }, [animal, setBreeds, setBreed]);
 
   return (
-    <div>
-      <form className="search-params">
+    <div className="search-params">
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          requestPets();
+        }}
+      >
         <label htmlFor="location">
           Location
           <input
@@ -35,6 +51,7 @@ const SearchParams = () => {
 
         <button>Submit</button>
       </form>
+      <Results pets={pets} />
     </div>
   );
 };
